af7237e8eaf6c27474308f70616708141231cb1e
TryToLiveAlon_owner <156423343+TryToLiveAlon@users.noreply.github.com>
2025-02-26 12:38:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from bot import Bot
 import pyrogram.utils
 
-pyrogram.utils.MIN_CHANNEL_ID = -1009147483647
+pyrogram.utils.MIN_CHANNEL_ID = -1002252585703
 
 if __name__ == "__main__":
     Bot().run()
